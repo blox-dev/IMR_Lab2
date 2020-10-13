dd0b4a85b95e02f5a5e15a5cceb9621ff30baa28
blox-dev <bloxorz51gaming@gmail.com>
2020-10-13 14:14:58 +0300

    Pls work

---
+++ Assets/VRTK/Examples/ExampleResources/SceneResources/[002 - Pointers] StraightPointer/Scripts/VRTKExample_PointerObjectHighlighterActivator.cs
@@ -0,0 +1,124 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using VRTK.Highlighters;
+
+    public class VRTKExample_PointerObjectHighlighterActivator : MonoBehaviour
+    {
+        public VRTK_DestinationMarker pointer;
+        public Color hoverColor = Color.cyan;
+        public Color selectColor = Color.yellow;
+        public bool logEnterEvent = true;
+        public bool logHoverEvent = false;
+        public bool logExitEvent = true;
+        public bool logSetEvent = true;
+
+        protected virtual void OnEnable()
+        {
+            pointer = (pointer == null ? GetComponent<VRTK_DestinationMarker>() : pointer);
+
+            if (pointer != null)
+            {
+                pointer.DestinationMarkerEnter += DestinationMarkerEnter;
+                pointer.DestinationMarkerExit += DestinationMarkerExit;
+                pointer.DestinationMarkerSet += DestinationMarkerSet;
+
+                pointer.DestinationMarkerHover += DestinationMarkerHover;
+            }
+            else
+            {
+                VRTK_Logger.Error(VRTK_Logger.GetCommonMessage(VRTK_Logger.CommonMessageKeys.REQUIRED_COMPONENT_MISSING_FROM_GAMEOBJECT, "VRTKExample_PointerObjectHighlighterActivator", "VRTK_DestinationMarker", "the Controller Alias"));
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (pointer != null)
+            {
+                pointer.DestinationMarkerEnter -= DestinationMarkerEnter; 
+                pointer.DestinationMarkerExit -= DestinationMarkerExit;
+                pointer.DestinationMarkerSet -= DestinationMarkerSet;
+
+                pointer.DestinationMarkerHover -= DestinationMarkerHover;
+            }
+        }
+
+        private void DestinationMarkerHover(object sender, DestinationMarkerEventArgs e)
+        {
+            if (logHoverEvent)
+            {
+                DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER HOVER", e.target, e.raycastHit, e.distance, e.destinationPosition);
+            }
+        }
+        protected virtual void DestinationMarkerEnter(object sender, DestinationMarkerEventArgs e)
+        {
+            ToggleHighlight(e.target, hoverColor);
+            if (logEnterEvent)
+            {
+                DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER ENTER", e.target, e.raycastHit, e.distance, e.destinationPosition);
+            }
+        }
+                protected virtual void DestinationMarkerExit(object sender, DestinationMarkerEventArgs e)
+                {
+                    ToggleHighlight(e.target, Color.clear);
+                    if (logExitEvent)
+                    {
+                        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER EXIT", e.target, e.raycastHit, e.distance, e.destinationPosition);
+                    }
+                }
+
+                protected virtual void DestinationMarkerSet(object sender, DestinationMarkerEventArgs e)
+                {
+                    ToggleHighlight(e.target, selectColor);
+                    if (logSetEvent)
+                    {
+                        DebugLogger(VRTK_ControllerReference.GetRealIndex(e.controllerReference), "POINTER SET", e.target, e.raycastHit, e.distance, e.destinationPosition);
+                    }
+                }
+        protected virtual void ToggleHighlight(Transform target, Color color)
+        {
+            VRTK_BaseHighlighter highligher = (target != null ? target.GetComponentInChildren<VRTK_BaseHighlighter>() : null);
+            if (highligher != null)
+            {
+                highligher.Initialise();
+                if (color != Color.clear)
+                {
+                    highligher.Highlight(color);
+                }
+                else
+                {
+                    highligher.Unhighlight();
+                }
+            }
+        }
+        private Vector3 last_pos;
+        private int lindex = 0;
+        protected virtual void DebugLogger(uint index, string action, Transform target, RaycastHit raycastHit, float distance, Vector3 tipPosition)
+        {
+            if (last_pos == Vector3.zero)
+                last_pos = tipPosition;
+            else
+            {
+                GameObject lineObject = new GameObject("Line" + lindex++);
+                lineObject.AddComponent<LineRenderer>();
+
+                LineRenderer line = lineObject.GetComponent<LineRenderer>();
+
+                line.positionCount = 2;
+                line.SetPosition(0, last_pos);
+                line.SetPosition(1, tipPosition);
+                line.startWidth = 0.1f;
+                line.endWidth = 0.1f;
+                line.useWorldSpace = true;
+                //line.material = LineMaterial;
+                last_pos = tipPosition;
+            }
+
+        
+
+            string targetName = (target ? target.name : "<NO VALID TARGET>");
+            string colliderName = (raycastHit.collider ? raycastHit.collider.name : "<NO VALID COLLIDER>");
+            VRTK_Logger.Info("Controller on index '" + index + "' is " + action + " at a distance of " + distance + " on object named [" + targetName + "] on the collider named [" + colliderName + "] - the pointer tip position is/was: " + tipPosition);
+        }
+    }
+}
